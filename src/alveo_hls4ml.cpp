9d0ac0623d4df932668991c229c2e1a5688472bc
YanLun-Huang <94730012+YanLunHuang@users.noreply.github.com>
2022-05-29 00:54:34 +0800

    has passed the software emulation

--- src/alveo_hls4ml.cpp
+++ src/alveo_hls4ml.cpp
@@ -41,136 +41,124 @@ Description:
 
 template<unsigned N> 
 void fillWeights(const model_default_t iWeightsIn[N], model_default_t weights[N]) { 
-  for(int i0 = 0; i0 < N; i0++) { 
-    weights[i0] = iWeightsIn[i0];
-  }
+	for(int i0 = 0; i0 < N; i0++) { 
+		weights[i0] = iWeightsIn[i0];
+	}
 }
 
-/*
-    HLS4ML Kernel Implementation 
-    Arguments:
-        in    (input)     --> Input Vector
-        out   (output)    --> Output Vector
-   */
 extern "C" {
 
 void alveo_hls4ml(
-        const bigdata_t *in, // Read-Only Vector
+	const bigdata_t *in, // Read-Only Vector
 	const model_default_t *in_weights1, 
 	const model_default_t *in_weights2, 
 	const model_default_t *in_weights3,
 	const model_default_t *in_weights4,
 	const model_default_t *in_weights5,
-        const model_default_t *in_weights6,
-        bigdata_t *out       // Output Result
-        )
+	const model_default_t *in_weights6,
+	bigdata_t *out       // Output Result
+	)
 {
-// SDAccel kernel must have one and only one s_axilite interface which will be used by host application to configure the kernel.
-// Here bundle control is defined which is s_axilite interface and associated with all the arguments (in and out),
-// control interface must also be associated with "return".
-// All the global memory access arguments must be associated to one m_axi(AXI Master Interface). Here all two arguments(in, out) are 
-// associated to bundle gmem which means that a AXI master interface named "gmem" will be created in Kernel and all these variables will be 
-// accessing global memory through this interface.
-// Multiple interfaces can also be created based on the requirements. For example when multiple memory accessing arguments need access to
-// global memory simultaneously, user can create multiple master interfaces and can connect to different arguments.
-#pragma HLS INTERFACE m_axi port=in  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights1  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights2  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights3  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights4  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights5  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in_weights6  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
-#pragma HLS INTERFACE s_axilite port=in   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights1   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights2   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights3   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights4   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights5   bundle=control
-#pragma HLS INTERFACE s_axilite port=in_weights6   bundle=control
-#pragma HLS INTERFACE s_axilite port=out  bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-    //necessary for hls4ml kernel, not used
-    #pragma HLS DATAFLOW
+	// SDAccel kernel must have one and only one s_axilite interface which will be used by host application to configure the kernel.
+	// Here bundle control is defined which is s_axilite interface and associated with all the arguments (in and out),
+	// control interface must also be associated with "return".
+	// All the global memory access arguments must be associated to one m_axi(AXI Master Interface). Here all two arguments(in, out) are 
+	// associated to bundle gmem which means that a AXI master interface named "gmem" will be created in Kernel and all these variables will be 
+	// accessing global memory through this interface.
+	// Multiple interfaces can also be created based on the requirements. For example when multiple memory accessing arguments need access to
+	// global memory simultaneously, user can create multiple master interfaces and can connect to different arguments.
+	#pragma HLS INTERFACE m_axi port=in  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights1  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights2  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights3  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights4  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights5  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=in_weights6  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
+	#pragma HLS INTERFACE s_axilite port=in   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights1   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights2   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights3   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights4   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights5   bundle=control
+	#pragma HLS INTERFACE s_axilite port=in_weights6   bundle=control
+	#pragma HLS INTERFACE s_axilite port=out  bundle=control
+	#pragma HLS INTERFACE s_axilite port=return bundle=control
+	//necessary for hls4ml kernel, not used
+	#pragma HLS DATAFLOW
 
 
-    static model_default_t w27[NW1];
-    static model_default_t w31[NW2];
-    static model_default_t w36[NW3];
-    static model_default_t w40[NW4];
-    static model_default_t w45[NW5];
-    static model_default_t w49[NW6];
-    //#pragma HLS RESOURCE variable=w27  core=XPM_MEMORY uram
-    //#pragma HLS RESOURCE variable=w31  core=XPM_MEMORY uram
-    #pragma HLS RESOURCE variable=w36  core=XPM_MEMORY uram
-    #pragma HLS RESOURCE variable=w40  core=XPM_MEMORY uram
-    #pragma HLS RESOURCE variable=w45  core=XPM_MEMORY uram
-    #pragma HLS RESOURCE variable=w49  core=XPM_MEMORY uram
-    static bool fillWeights_ = false;
-
-    if(!fillWeights_) { 
-      fillWeights<NW1>(in_weights1,w27);
-      fillWeights<NW2>(in_weights2,w31);
-      fillWeights<NW3>(in_weights3,w36);
-      fillWeights<NW4>(in_weights4,w40);
-      fillWeights<NW5>(in_weights5,w45);
-      fillWeights<NW6>(in_weights6,w49);
-      fillWeights_ = true;
-    } else {
-
-      bigdata_t in_bigbuf[BIGSTREAMSIZE_IN*STREAMSIZE];
-      bigdata_t out_bigbuf[BIGSTREAMSIZE_OUT*STREAMSIZE];
-      
-      hls::stream<input_t> in_buf[STREAMSIZE][DATA_SIZE_IN+1];
-      hls::stream<result_t> out_buf[STREAMSIZE][DATA_SIZE_OUT];
-      //these will get partitioned properly in the hls4ml code
-  
-      #pragma HLS ARRAY_PARTITION   variable=in_buf  complete dim=0
-      #pragma HLS ARRAY_PARTITION   variable=out_buf complete dim=0
-      #pragma HLS STREAM   variable=in_buf  depth=1000
-      #pragma HLS STREAM   variable=out_buf depth=1
-  
-      //getting data from DDR
-      for (int i = 0; i < STREAMSIZE*BIGSTREAMSIZE_IN; i++) {
-        #pragma HLS LOOP UNROLL
-        in_bigbuf[i] = in[i];
-      }
-      for (int i = 0; i < STREAMSIZE; i++) {
-        std::cout<<"------------------"<<std::endl;
-        for(int i0 = 0; i0 < IN_STREAM_LEN; i0++) { 
-	  for(int i1 = 0; i1 < DATA_SIZE_IN; i1++) { 
-	    #pragma HLS UNROLL
-	    int ib = (i0*DATA_SIZE_IN+i1)%COMPRESSION;
-	    int ia = i*BIGSTREAMSIZE_IN+( (i0*DATA_SIZE_IN+i1)/COMPRESSION);
-	    input_t tmp;
-	    tmp.range(15,0) = in_bigbuf[ia].range(16*(ib+1)-1,16*ib);
-	    int chan = ( (i0*DATA_SIZE_IN+i1) % DATA_SIZE_IN)+1;
-	    if (chan == 1) in_buf[i][0].write((input_t)(i0 == 0 ? 0 : 1));
-	    in_buf[i][chan].write(tmp);
-	    std::cout<<"writing to ["<<i<<"]["<<chan<<"]"<<std::endl;
-	  }
+	static model_default_t w27[NW1];
+	static model_default_t w31[NW2];
+	static model_default_t w36[NW3];
+	static model_default_t w40[NW4];
+	static model_default_t w45[NW5];
+	static model_default_t w49[NW6];
+	#pragma HLS RESOURCE variable=w27  core=XPM_MEMORY uram
+	#pragma HLS RESOURCE variable=w31  core=XPM_MEMORY uram
+	#pragma HLS RESOURCE variable=w36  core=XPM_MEMORY uram
+	#pragma HLS RESOURCE variable=w40  core=XPM_MEMORY uram
+	#pragma HLS RESOURCE variable=w45  core=XPM_MEMORY uram
+	#pragma HLS RESOURCE variable=w49  core=XPM_MEMORY uram
+	
+	//load for the first time
+	static bool fillWeights_ = false;
+	if(!fillWeights_) {
+		fillWeights<NW1>(in_weights1,w27);
+		fillWeights<NW2>(in_weights2,w31);
+		fillWeights<NW3>(in_weights3,w36);
+		fillWeights<NW4>(in_weights4,w40);
+		fillWeights<NW5>(in_weights5,w45);
+		fillWeights<NW6>(in_weights6,w49);
+		fillWeights_ = true;
 	}
-        std::cout<<"inf start"<<std::endl;
-        hls4ml: MYPROJ(in_buf[i],out_buf[i],w27,w31,w36,w40,w45,w49);
+	bigdata_t in_bigbuf[DATA_SIZE_IN*IN_STREAM_LEN];
+	bigdata_t out_bigbuf;
+	
+	hls::stream<input_t> in_buf[DATA_SIZE_IN];
+	hls::stream<result_t> out_buf[DATA_SIZE_OUT];
+	//these will get partitioned properly in the hls4ml code
+
+	#pragma HLS ARRAY_PARTITION   variable=in_buf  complete dim=0
+	#pragma HLS ARRAY_PARTITION   variable=out_buf complete dim=0
+	#pragma HLS STREAM   variable=in_buf  depth=1000
+	#pragma HLS STREAM   variable=out_buf depth=1
+	
+	//getting data from DRAM
+	for (int i = 0; i < DATA_SIZE_IN*IN_STREAM_LEN; i++) {
+		#pragma HLS LOOP UNROLL
+		in_bigbuf[i] = in[i];
+	}
+	
+	std::cout<<"------------------"<<std::endl;
+	//=============================================
+	//input
+	//=============================================
+	input_t tmp;
+	for(int i0 = 0; i0 < IN_STREAM_LEN; i0++) { 
+		for(int i1 = 0; i1 < DATA_SIZE_IN; i1++) { 
+			#pragma HLS UNROLL
+			tmp = in_bigbuf[i0*4+i1];
+			in_buf[i1].write(tmp);
+			std::cout<<double(tmp)<<std::endl;
+		}
+	}
+	std::cout<<"inf start"<<std::endl;
+	hls4ml: MYPROJ(in_buf,out_buf,w27,w31,w36,w40,w45,w49);
 	std::cout<<"inf end"<<std::endl;
-        bigdata_t tmp;
-        for(int i0 = 0; i0 < OUT_STREAM_LEN; i0++) { 
-         for(int i1 = 0; i1 < DATA_SIZE_OUT; i1++) {
-	  #pragma HLS UNROLL
-          int ib = (i0*DATA_SIZE_OUT+i1)%COMPRESSION;
-          int ia = i*BIGSTREAMSIZE_OUT+((i0*DATA_SIZE_OUT+i1)/COMPRESSION);
-          std::cout<<"reading from ["<<i<<"]["<<i1<<"]"<<std::endl;
-          result_t tmp_small = out_buf[i][i1].read();
-          tmp((ib+1)*16-1,(ib)*16) = tmp_small.range(15,0);
-          if (((i0*DATA_SIZE_OUT+i1)%COMPRESSION == COMPRESSION-1) || (i0 == OUT_STREAM_LEN-1 && i1 == DATA_SIZE_OUT-1)) out_bigbuf[ia] = tmp;
-         }
-        }
-      }
-      //place output into DDR
-      for (int i = 0; i < STREAMSIZE*BIGSTREAMSIZE_OUT; i++) {
-        #pragma HLS LOOP UNROLL
-        out[i] = out_bigbuf[i];
-      }
-    }
+
+	//=============================================
+	//output
+	//=============================================
+	for(int i1 = 0; i1 < DATA_SIZE_OUT; i1++) {
+		//#pragma HLS UNROLL
+		std::cout<<"reading from ["<<i1<<"]"<<std::endl;
+		result_t tmp_small = out_buf[i1].read();
+		out_bigbuf = tmp_small;
+	}
+
+	out[0] = out_bigbuf;
+	std::cout <<(double)out_bigbuf<<std::endl;
+
 }
 }
